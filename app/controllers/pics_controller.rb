fd83d03f83e6024a616e5d40a3d3d6214695266c
vagrant <vagrant@precise32.(none)>
2014-02-15 22:47:08 +0000

    new forms

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,4 +4,19 @@ class PicsController < ApplicationController
 		@pics = Pic.all
 	end
 
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		Pic.create( pic_params )
+		redirect_to pics_path
+	end
+
+	private 
+
+	def pic_params
+		params.require(:pic).permit(:title, :message, :who)
+	end
+
 end
