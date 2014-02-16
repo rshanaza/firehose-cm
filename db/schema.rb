ab8049dfa01996d88e83257b007e57548443aa43
vagrant <vagrant@precise32.(none)>
2014-02-16 18:48:16 +0000

    online

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140215200354) do
+ActiveRecord::Schema.define(version: 20140216170614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140215200354) do
     t.string   "who"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
